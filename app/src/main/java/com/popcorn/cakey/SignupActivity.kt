515d67546e1cb652d825eaf6ceaad4b15edd59ca
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-06-14 16:37:59 +0700

    Update SignupActivity.kt

--- app/src/main/java/com/popcorn/cakey/SignupActivity.kt
+++ app/src/main/java/com/popcorn/cakey/SignupActivity.kt
@@ -7,15 +7,18 @@ import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.FirebaseDatabase
 import com.popcorn.cakey.databinding.ActivitySignupBinding
 
-data class User(val username: String, val email: String)
-
 class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
-
+    private lateinit var username: String
+    private lateinit var email: String
+    private lateinit var pwd: String
+    private lateinit var cPwd: String
     private lateinit var fireAuth: FirebaseAuth
+    private lateinit var fireDB: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,61 +33,62 @@ class SignupActivity : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         fireAuth = FirebaseAuth.getInstance()
+        fireDB = FirebaseDatabase.getInstance()
 
         binding.btnSignup.setOnClickListener {
-            val username = binding.etUsername.text.toString().trim()
-            val email = binding.etEmail.text.toString().trim()
-            val pwd = binding.etPassword.text.toString().trim()
-            val cPwd = binding.etConfirmPassword.text.toString().trim()
+            username = binding.etUsername.text.toString().trim()
+            email = binding.etEmail.text.toString().trim()
+            pwd = binding.etPassword.text.toString().trim()
+            cPwd = binding.etConfirmPassword.text.toString().trim()
             // Check if any field is empty, validate email and confirm password
             when {
                 TextUtils.isEmpty(username) -> {
                     showToast(R.string.username_required)
                 }
-
                 TextUtils.isEmpty(email) -> {
                     showToast(R.string.email_required)
                 }
-
                 (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) -> {
                     showToast(R.string.email_invalid)
                 }
-
                 TextUtils.isEmpty(pwd) -> {
                     showToast(R.string.password_required)
                 }
-
                 (pwd.length < resources.getInteger(R.integer.password_min_length)) -> {
                     showToast(R.string.password_too_short)
                 }
-
                 (pwd != cPwd) -> {
                     showToast(R.string.password_mismatch)
                 }
-
                 else -> {
-                    binding.progressBar.visibility = View.VISIBLE
+                    blockSignup()
                     fireAuth.createUserWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(this) { taskAuth ->
-                            if (taskAuth.isSuccessful) {
-                                val user = User(username, email)
-                                val uid = fireAuth.currentUser!!.uid
-
-                                FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(uid).setValue(user)
-                                    .addOnCompleteListener(this) { task ->
-                                        if (task.isSuccessful) {
-                                            showToast(getString(R.string.signup_succeed, username))
-                                        } else {
-                                            showToast(R.string.signup_failed)
-                                        }
+                        // Bind listener life cycle to this activity, to prevent activity leak
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                val profileChange = UserProfileChangeRequest.Builder()
+                                    .setDisplayName(username).build()
+                                fireAuth.currentUser!!.updateProfile(profileChange)
+                                    .addOnSuccessListener(this) {
+                                        showToast(getString(R.string.signup_succeed, username))
                                     }
-                            }
-                            binding.progressBar.visibility = View.GONE
+                            } else showToast(R.string.signup_failed)
+                            unblockSignup()
                         }
                 }
             }
+
         }
+    }
+
+    private fun blockSignup() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnSignup.isEnabled = false
+    }
+
+    private fun unblockSignup() {
+        binding.progressBar.visibility = View.GONE
+        binding.btnSignup.isEnabled = true
     }
 
     private fun showToast(resId: Int) {
