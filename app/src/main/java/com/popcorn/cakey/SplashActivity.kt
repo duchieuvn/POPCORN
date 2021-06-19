4307d63919206591d32ce9ce63751808f31f699a
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-06-19 18:08:46 +0700

    replace DEPRECATED function

--- app/src/main/java/com/popcorn/cakey/SplashActivity.kt
+++ app/src/main/java/com/popcorn/cakey/SplashActivity.kt
@@ -4,24 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
 
-    private val splash_time : Long = 3000
+    private val splashTime : Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        @Suppress("DEPRECATION")
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
             finish()
-        }, splash_time)
-
-
+        }, splashTime)
     }
 }
