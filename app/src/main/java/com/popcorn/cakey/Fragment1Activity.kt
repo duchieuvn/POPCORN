35b6cdd5c4a5df943d7c1e755ae212d2d966856c
Truong19clc5 <nguyentattruong0104@gmail.com>
2021-07-04 20:57:01 +0700

    update

--- app/src/main/java/com/popcorn/cakey/Fragment1Activity.kt
+++ app/src/main/java/com/popcorn/cakey/Fragment1Activity.kt
@@ -1,19 +1,74 @@
 package com.popcorn.cakey
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class Fragment1Activity: Fragment(R.layout.activity_fragment1) {
+
+class Fragment1Activity: Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerApdater.ViewHolder>? = null
     companion object{
         fun newInstance():Fragment1Activity{
             return Fragment1Activity()
         }
     }
     @Override
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        layoutManager= LinearLayoutManager(this.context,LinearLayoutManager.HORIZONTAL,false)
+        recyclerView.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager=layoutManager
+        recyclerView.adapter=RecyclerApdater()
+
+        val view: View=inflater.inflate(R.layout.activity_suggestion,container,false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        R.id.recyclerView.apply{
+            layoutManager=LinearLayoutManager(activity)
+            adapter=RecyclerApdater()
+        }
+    }
+}
+class RecyclerApdater: RecyclerView.Adapter<RecyclerApdater.ViewHolder>(){
+
+    private var title= arrayOf("ID1", "ID2","ID3", "ID4")
+    private var image= intArrayOf(R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar)
+
+    fun RecyclerApdater(){
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerApdater.ViewHolder {
+        val v= LayoutInflater.from(parent.context).inflate(R.layout.activity_suggestion,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerApdater.ViewHolder, position: Int) {
+        holder.itemTitle.text=title[position]
+        holder.itemImage.setImageResource(image[position])
+    }
+
+    override fun getItemCount(): Int {
+        return title.size
+    }
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+
+        init{
+            itemImage=itemView.findViewById(R.id.thumnail)
+            itemTitle=itemView.findViewById(R.id.username)
+        }
     }
 }
--- app/src/main/java/com/popcorn/cakey/MainActivity.kt
+++ app/src/main/java/com/popcorn/cakey/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.popcorn.cakey
 
 import android.annotation.SuppressLint
-import android.app.Fragment
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 
 class MainActivity: AppCompatActivity(R.layout.activity_main) {
     private lateinit var menuBar: androidx.appcompat.widget.Toolbar
-    private lateinit var f1: Fragment1Activity
-    private lateinit var f2: Fragment2Activity
-    private  lateinit var fm: FragmentManager
-    private  lateinit var ft: FragmentTransaction
+
 
     @SuppressLint("UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +19,10 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
 
         if (savedInstanceState==null)
         {
-            supportFragmentManager.beginTransaction().commit()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.one_fragment,Fragment1Activity.newInstance())
+                .add(R.id.two_fragment,Fragment2Activity.newInstance())
+                .commit()
         }
     }
 
