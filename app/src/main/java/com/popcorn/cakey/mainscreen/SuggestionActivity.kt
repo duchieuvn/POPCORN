37b3febdd13cf0e8bce0cfcd687c51c2cb83941a
Truong19clc5 <nguyentattruong0104@gmail.com>
2021-07-11 19:57:49 +0700

    update main screen

--- app/src/main/java/com/popcorn/cakey/mainscreen/SuggestionActivity.kt
+++ app/src/main/java/com/popcorn/cakey/mainscreen/SuggestionActivity.kt
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.popcorn.cakey.R
 
 class SuggestionActivity:RecyclerView.Adapter<SuggestionActivity.ViewHolder>() {
-    private var title= arrayOf("anh1", "anh2","anh3", "anh4","anh5", "anh6","anh7", "anh8")
     private var image= intArrayOf(
         R.drawable.avatar, R.drawable.avatar, R.drawable.avatar,
         R.drawable.avatar, R.drawable.avatar, R.drawable.avatar, R.drawable.avatar, R.drawable.avatar)
@@ -20,17 +19,15 @@ class SuggestionActivity:RecyclerView.Adapter<SuggestionActivity.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder:SuggestionActivity.ViewHolder, position: Int) {
-        holder.itemTitle.text=title[position]
         holder.itemImage.setImageResource(image[position])
     }
 
     override fun getItemCount(): Int {
-        return title.size
+        return image.size
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         var itemImage: ImageView = itemView.findViewById(R.id.thumnail)
-        var itemTitle: TextView = itemView.findViewById(R.id.username)
 
     }
 }
