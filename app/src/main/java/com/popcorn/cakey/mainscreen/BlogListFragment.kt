b41fcb1d64bb308aae195cd3f23c21943fbf3991
Truong19clc5 <nguyentattruong0104@gmail.com>
2021-08-21 20:02:40 +0700

    update

--- app/src/main/java/com/popcorn/cakey/mainscreen/BlogListFragment.kt
+++ app/src/main/java/com/popcorn/cakey/mainscreen/BlogListFragment.kt
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.parse.ParseObject
 import com.parse.ParseQuery
-import com.parse.ktx.findAll
 import com.popcorn.cakey.R
 
 
@@ -19,8 +18,8 @@ class BlogListFragment: Fragment(R.layout.activity_fragment2) {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<BlogListActivity.ViewHolder>? = null
     private lateinit var title: ArrayList<String>
+    //private lateinit var image: ArrayList<ParseObject>
     private lateinit var image: Array<Int>
-
     companion object{
         fun newInstance(): BlogListFragment {
            return BlogListFragment()
@@ -30,15 +29,15 @@ class BlogListFragment: Fragment(R.layout.activity_fragment2) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view: View=inflater.inflate(R.layout.activity_fragment2,container,false)
         val queryBlog= ParseQuery.getQuery<ParseObject>("Blog").setLimit(8)
-        val data = queryBlog.find()
+        val data = queryBlog?.find()
         title=ArrayList()
-        for (item in data.indices){
-            title.add(data[item].getString("name").toString())
+        for (i in data?.indices!!){
+            title.add(data[i].getString("name").toString())
         }
 
 
         image= arrayOf(
-            R.drawable.avatar,
+           R.drawable.avatar,
             R.drawable.avatar,
             R.drawable.avatar,
             R.drawable.avatar,
