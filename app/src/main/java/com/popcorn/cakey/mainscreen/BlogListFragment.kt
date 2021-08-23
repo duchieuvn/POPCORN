0f0673f76d238af997fa14c1a3e8350dbf27f9bb
Truong19clc5 <nguyentattruong0104@gmail.com>
2021-08-23 13:00:16 +0700

    Update BlogListFragment.kt

--- app/src/main/java/com/popcorn/cakey/mainscreen/BlogListFragment.kt
+++ app/src/main/java/com/popcorn/cakey/mainscreen/BlogListFragment.kt
@@ -3,10 +3,13 @@ package com.popcorn.cakey.mainscreen
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,7 +27,7 @@ class BlogListFragment: Fragment(R.layout.activity_fragment2) {
     private var adapter: RecyclerView.Adapter<BlogListActivity.ViewHolder>? = null
     private lateinit var title: ArrayList<String>
     private lateinit var image: ArrayList<ParseFile>
-    private val meo=R.drawable.avatar
+    private val mew=R.drawable.avatar
     private lateinit var blogid: ArrayList<String>
     companion object{
         fun newInstance(): BlogListFragment {
@@ -36,28 +39,27 @@ class BlogListFragment: Fragment(R.layout.activity_fragment2) {
     @Override
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view: View=inflater.inflate(R.layout.activity_fragment2,container,false)
-        val queryBlog= ParseQuery.getQuery<ParseObject>("Blog").setLimit(20)
+        val queryBlog= ParseQuery.getQuery<ParseObject>("Blog").setLimit(50)
         val data = queryBlog?.find()
         title=ArrayList()
         image=ArrayList()
         blogid=ArrayList()
-        val icon=BitmapFactory.decodeResource(resources,meo)
+        // null picture
+        val icon=BitmapFactory.decodeResource(resources,mew)
         val stream= ByteArrayOutputStream()
         icon.compress(Bitmap.CompressFormat.PNG,100,stream)
         val byte= stream.toByteArray()
         val temp =ParseFile(byte)
+        temp.saveInBackground()
 
         for (i in data?.indices!!){
             val pics=data[i].getParseFile("img")
-
+            title.add(data[i].getString("name").toString())
+            blogid.add(data[i].objectId)
             if(pics!=null){
                 image.add(pics)
-                title.add(data[i].getString("name").toString())
-                blogid.add(data[i].objectId)
             }
             else{
-                title.add(data[i].getString("name").toString())
-                blogid.add(data[i].objectId)
                 image.add(temp)
             }
 
