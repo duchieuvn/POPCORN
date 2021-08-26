56c980ca453b2c60d3779cb88029dc272b6a617f
nguyendothanhtruc <trucnguyen0256@gmail.com>
2021-08-26 11:05:59 +0700

    update stream video (new)

--- app/src/main/java/com/popcorn/cakey/blog/ReadBlogActivity.kt
+++ app/src/main/java/com/popcorn/cakey/blog/ReadBlogActivity.kt
@@ -1,12 +1,15 @@
 package com.popcorn.cakey.blog
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,10 +19,15 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import com.google.android.material.textfield.TextInputEditText
 import com.parse.*
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.popcorn.cakey.R
 import com.popcorn.cakey.databinding.ActivityReadBlogBinding
 import com.popcorn.cakey.report.ReportActivity
@@ -56,6 +64,12 @@ class ReadBlogActivity : AppCompatActivity() {
     private lateinit var defaultLike: String
     private lateinit var defaultDislike: String
 
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+    private lateinit var videoLink: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_read_blog)
@@ -245,36 +259,77 @@ class ReadBlogActivity : AppCompatActivity() {
         }
 
         //Youtube
-        val videoLink = blogContent.getString("videoUrl")
+        videoLink = blogContent.getString("videoUrl")!!
 
-        val youTubePlayerView = binding.youTubePlayerView
-        lifecycle.addObserver(youTubePlayerView)
-
-        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(youTubePlayer: YouTubePlayer) {
-                if (videoLink != null) {
-                    val videoId = extractYTId(videoLink)
-                    if (videoId != null) {
-                        youTubePlayer.loadVideo(videoId, 0f)
-                    }
-                }
-            }
-        })
+        playerView = binding.playerView
 
     }
+    private fun initPlayer(){
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+        // Bind the player to the view.
+        playerView.player = mPlayer
+        mPlayer!!.playWhenReady = true
+        mPlayer!!.seekTo(playbackPosition)
+        mPlayer!!.prepare(buildMediaSource(), false, false)
+    }
+    private fun buildMediaSource(): MediaSource {
+        val userAgent =
+            Util.getUserAgent(playerView.context, playerView.context.getString(R.string.app_name))
 
-    fun extractYTId(ytUrl: String?): String? {
-        var vId: String? = null
-        val pattern: Pattern = Pattern.compile(
-            "^https?://.*(?:youtu.be/|v/|u/\\w/|embed/|watch?v=)([^#&?]*).*$",
-            Pattern.CASE_INSENSITIVE
-        )
-        val matcher: Matcher = pattern.matcher(ytUrl)
-        if (matcher.matches()) {
-            vId = matcher.group(1)
+        val dataSourceFactory = DefaultHttpDataSourceFactory(userAgent)
+
+        return ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(videoLink))
+
+    }
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
         }
-        return vId
+        playWhenReady = mPlayer!!.playWhenReady
+        playbackPosition = mPlayer!!.currentPosition
+        currentWindow = mPlayer!!.currentWindowIndex
+        mPlayer!!.release()
+        mPlayer = null
     }
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
 
     private fun like(blog: ParseObject) {
         if (likeClick) {
